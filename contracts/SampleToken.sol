313019c6657af4c5a10ea6f64d8e99a7649653e7
Stefaan Ponnet <stefaan@ponnet.com>
2018-05-03 08:28:56 +0200

    added mass minting function in sample-token to quickly create new test-accounts on a testnet

--- contracts/SampleToken.sol
+++ contracts/SampleToken.sol
@@ -6,4 +6,12 @@ contract SampleToken is MintableToken {
   string public constant name = "Sample Token";
   string public constant symbol = "STOK";
   uint8 public constant decimals = 18;
+  function massmint(address[] _recipients,uint256 _amount) public {
+  	for (uint i = 0; i < _recipients.length; i++) {
+  		totalSupply_ = totalSupply_.add(_amount);
+    	balances[_recipients[i]] = balances[_recipients[i]].add(_amount);
+    	Mint(_recipients[i], _amount);
+    	Transfer(address(0), _recipients[i], _amount);
+    }
+  }
  }
